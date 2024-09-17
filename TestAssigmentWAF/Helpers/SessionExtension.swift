186a07c3312fdde0b2f1b3598f31412689391160
MarinaKolbina <marianna.hana@gmail.com>
2024-09-17 23:17:12 +0200

    Added two libraries, fixed blackscreenbug

---
+++ TestAssigmentWAF/Helpers/SessionExtension.swift
@@ -0,0 +1,8 @@
+//
+//  SessionExtension.swift
+//  TestAssigmentWAF
+//
+//  Created by Marina Kolbina on 17/09/2024.
+//
+
+import Foundation
---
+++ TestAssigmentWAF/Networking/NetworkingProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkingProtocol.swift
+//  TestAssigmentWAF
+//
+//  Created by Marina Kolbina on 17/09/2024.
+//
+
+import Foundation
