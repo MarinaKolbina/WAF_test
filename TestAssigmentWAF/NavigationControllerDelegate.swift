490eae37565436d03461932ab773530cf39fee71
MarinaKolbina <marianna.hana@gmail.com>
2024-09-17 14:47:27 +0200

    Fixed animations

---
+++ TestAssigmentWAF/NavigationControllerDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationControllerDelegate.swift
+//  TestAssigmentWAF
+//
+//  Created by Marina Kolbina on 17/09/2024.
+//
+
+import Foundation
---
+++ TestAssigmentWAF/SlideInTransition.swift
@@ -0,0 +1,8 @@
+//
+//  SlideInTransition.swift
+//  TestAssigmentWAF
+//
+//  Created by Marina Kolbina on 17/09/2024.
+//
+
+import Foundation
